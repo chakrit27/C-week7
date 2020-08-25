aef249fcc16983394d4651e387163c455b36fa13
chakrit27 <69183220+chakrit27@users.noreply.github.com>
2020-08-25 11:57:16 +0700

    Add files via upload

---
+++ myC702.c
@@ -0,0 +1,20 @@
+/*
+Program : c-output
+Authority : Mr.Chakrit
+Date : 8/25/63
+*/
+#include<stdio.h>
+int main(){
+    int testInteger = 5;
+    float number1 = 13.5;
+    double number2 = 12.4;
+    char chr = 'a';
+    //Displays the string inside quotations
+    printf("c-programming\n");
+    printf("--------\n");
+    printf("Number = %d\n", testInteger);
+    printf("number1 = %f\n",number1);
+    printf("number2 = %f\n",number2);
+    printf("charactor = %c\n", chr);
+    return 0;
+}
